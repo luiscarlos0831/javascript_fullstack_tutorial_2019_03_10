45d877fddbdc2e5f1c8989e43abfb943e380d79d
luis <luiscarlos_0831@hotmail.com>
2019-03-10 13:36:01 -0500

    tutorial js fullstack

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, 'public/uploads'),
+  filename(req, file, cb) {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  }
+});
+app.use(multer({ storage }).single('image'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use('/api/books', require('./routes/books'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
+});
